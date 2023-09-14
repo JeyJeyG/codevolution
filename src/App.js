d22957a197af36e77edc112ad902cd79f18c09ad
JeyJeyG <jeyjeygraham@gmail.com>
2023-09-14 15:36:17 +0000

    Props element

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import Greet from './components/Greet';
-import Welcome from './components/Welcome';
-import Hello from './components/Hello';
+import logo from "./logo.svg";
+import "./App.css";
+import Greet from "./components/Greet";
+import Welcome from "./components/Welcome";
+import Hello from "./components/Hello";
 function App() {
   return (
     <div className="App">
-      <Greet name = "Jeffery"/>
-      <Greet name ="Erica"/>
-      <Greet name ="Gena"/>
-      <Greet name ="Clark"/>
-      {/* <Welcome /> */}
+      <Greet name="Jeffery" heroName="The Scorpion King" />
+      <p> This is children props</p>
+      <Greet name="Erica" heroName="Catwoman" />
+      <button>Action</button>
+      <Greet name="Gena" heroName="Sonya" />
+      <Greet name="Clark" heroName="Superman" />
+      <Welcome name="Jeffery" heroName="The Scorpion King"/>
+      <Welcome name="Erica" heroName="Catwoman"/>
+      <Welcome name="Gena" heroName="Sonya"/>
       {/* <Hello /> */}
-      
     </div>
   );
 }
